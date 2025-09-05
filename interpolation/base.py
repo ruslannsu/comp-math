fd25a8460f0c643ed83c56552299b8179888a479
ruslan <ruslanone57@gmail.com>
2025-09-05 22:08:08 +0700

    add: base class

---
+++ interpolation/base.py
@@ -0,0 +1,11 @@
+class Base:
+    def __init__(self, data_size):
+        self.data_size = data_size
+        self.coef_ = [0] * (self.data_size - 1)
+
+    def fit(self, x_values, y_values):
+        pass
+        
+
+
+        
---
+++ interpolation/lagrange.py
@@ -0,0 +1,13 @@
+from base import Base
+
+class LagrangePolynomial(Base):
+    def __init__(self, data_size):
+        super().__init__(data_size)
+
+    def fit(self, x_values, y_values):
+        if len(x_values) != self.data_size or len(y_values != self.data_size):
+            return None
+        
+
+        
+        
